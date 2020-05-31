5fbab67ebf65accfec238dc2ffa10c6d52d1b88c
douglasbrandao21 <douglasbrando5@gmail.com>
2020-05-31 17:04:54 -0300

    refatoracao finalizada

--- src/slave.c
+++ src/slave.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,22 +11,20 @@
 #include <arpa/inet.h>
 #include <math.h>
 
-/* Defines the server port */
 #define PORT 5000
 
-/* Sockets buffers length */
 #define LEN 64
 
-/* Server address */
 #define SERVER_ADDR "127.0.0.1"
 
-double calculateIntegral( double intervalo, double discretization){
+double calculateIntegral( double interval, double discretization){
     double total = 0;
     double point1 = 0;
     double point2 = 0;
 
-    point1 = sqrt(pow(100, 2) - pow(intervalo,2));
-    point2 = sqrt(pow(100, 2) - pow((intervalo + discretization), 2));
+    // Regra do trapezio
+    point1 = sqrt(pow(100, 2) - pow(interval,2));
+    point2 = sqrt(pow(100, 2) - pow((interval + discretization), 2));
        
     total = (point1 + point2)*discretization/2;
 
@@ -35,36 +32,28 @@ double calculateIntegral( double intervalo, double discretization){
 }
 
 
-/*
- * Main execution of the client program of our simple protocol
- */
 int main(int argc, char *argv[]) {
 
-    /* Server socket */
     struct sockaddr_in server;
-    /* Client file descriptor for the local socket */
-    int sockfd;
+
+    int local_socket_file_descriptor;
 
     int len = sizeof(server);
     int slen;
 
-    /* Receive buffer */
     char buffer_in[LEN];
-    /* Send buffer */
     char buffer_out[LEN];
 
     fprintf(stdout, "Starting Slave ...\n");
 
-    /*
-     * Creates a socket for the client
-     */
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    // Cria o socket para o slave
+    if ((local_socket_file_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("Error on slave socket creation:");
+        
         return EXIT_FAILURE;
     }
 
-
-    /* Defines the connection properties */
+    // Propriedades da conexao
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
     server.sin_addr.s_addr = inet_addr(SERVER_ADDR);
@@ -72,53 +61,50 @@ int main(int argc, char *argv[]) {
     memset(server.sin_zero, 0x0, 8);
 
 
-    /* Tries to connect to the server */
-    if (connect(sockfd, (struct sockaddr*) &server, len) == -1) {
+    // Tenta se conectar ao master
+    if (connect(local_socket_file_descriptor, (struct sockaddr*) &server, len) == -1) {
         perror("Can't connect to server");
         return EXIT_FAILURE;
     }
 
-    double intervalo = 0;
+    double interval = 0;
     double discretization = 0;
-    double resultado = 0;
+    double result = 0;
     
-    /* Receives the presentation message from the server */
-    if ((slen = recv(sockfd, buffer_in, LEN, 0)) > 0) {
-        
+    // Recebe a mensagem de apresentacao do master
+    if ((slen = recv(local_socket_file_descriptor, buffer_in, LEN, 0)) > 0)
          sscanf(buffer_in, "%lf", &discretization);
-    }
 
     strcpy(buffer_out, "ready");
-    send(sockfd, buffer_out, strlen(buffer_out), 0);
+    
+    send(local_socket_file_descriptor, buffer_out, strlen(buffer_out), 0);
 
-    /*
-     * Commuicate with the server until the exit message come
-     */
+    // Mantem a comunicacao com o master aberta enquanto a mensagem "exit"
+    // nao chegar
     while (true) {
-        /* Zeroing the buffers */
+        // Zerando os buffers
         memset(buffer_in, 0x0, LEN);
         memset(buffer_out, 0x0, LEN);
 
-        /* Receives an answer from the server */
-        slen = recv(sockfd, buffer_in, LEN, 0);
+        // Recebe a resposta do servidor
+        slen = recv(local_socket_file_descriptor, buffer_in, LEN, 0);
         
-        sscanf(buffer_in, "%lf", &intervalo);
-        resultado = calculateIntegral(intervalo, discretization);
+        sscanf(buffer_in, "%lf", &interval);
 
-        /* 'bye' message finishes the connection */
+        result = calculateIntegral(interval, discretization);
+
+        // A mensagem de "bye" fecha a conexao
         if(strcmp(buffer_in, "bye") == 0)
             break;
         
-        sprintf(buffer_out, "%lf", resultado);
+        sprintf(buffer_out, "%lf", result);
 
-        /* Sends the read message to the server through the socket */
-        send(sockfd, buffer_out, strlen(buffer_out), 0);
+        // Envia a mensagem para o servidor por meio do socket
+        send(local_socket_file_descriptor, buffer_out, strlen(buffer_out), 0);
     }
 
-    /* Close the connection whith the server */
-    close(sockfd);
-
-    //fprintf(stdout, "\nConnection closed\n\n");
+    // Encerra a conexao com o master
+    close(local_socket_file_descriptor);
 
     return 0;
 }   
